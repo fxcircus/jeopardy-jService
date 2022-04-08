fe236c9c3fad9493eeb11f704c84f31914000a8c
Roy Daniel <roydanielr@gmail.com>
2022-04-08 17:51:52 -0400

    print questions when load

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,25 +1,30 @@
 import { useState, useEffect } from "react"
 
 export default function Question(props) {
-    const [category, setCategory] = useState('')
+    const [category, setCategory] = useState("")
     const [points, setPoints] = useState(0)
     const [answer, setAnswer] = useState('')
-
     const url = `http://jservice.io/api/random`
 
-    // const getQuestion = async () => {
-    //     try {
-    //         const 
-    //     }
-    // }
+    const getQuestion = async () => {
+        try {
+            const response = await fetch(url)
+            const data = await response.json()
+            setCategory(data[0].category.title)
+            setPoints(data[0].value)
+            setAnswer(data[0].answer)
+        } catch(e){
+            console.error(e)
+        }
+    }
 
     useEffect(() => {
-
+        getQuestion()
     }, [])
 
     return (
         <div>
-            <h2>category: {category}</h2>
+            <h2>Category: {category}</h2>
             <h3>Points: {points}</h3>
             <h3>Answer: {answer}</h3>
         </div>
